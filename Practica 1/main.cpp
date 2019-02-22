0b02258e1222fe58865ed6dccb28edc130b89fbd
SenyorTerry <37777549+SenyorTerry@users.noreply.github.com>
2019-02-22 09:50:41 +0100

    Práctica final

--- Practica 1/main.cpp
+++ Practica 1/main.cpp
@@ -8,28 +8,27 @@ int Contar(int [], int, int);
 int main()
 {
     int x = 0;
-    int tam = 0;
+    int n = 0;
     cout << "----------------------------------------\n";
     cout << "Valor a buscar: ";
     cin >> x;
     cout << "----------------------------------------\n";
     cout << "Tamano de los vectores: ";
-    cin >> tam;
+    cin >> n;
     cout << "----------------------------------------\n";
-    cout << "Tam: " << tam << endl;
 
-    int vpeor[tam];
-    int vmejor[tam];
-    int vcualquiera[tam];
+    int vpeor[n];
+    int vmejor[n];
+    int vcualquiera[n];
 
     //Completamos el array con los datos
-    for(int i = 1; i <= tam; i++){
+    for(int i = 1; i <= n; i++){
         //Vector igual al elemento a buscar
         vpeor[i] = x;
         //Vector diferente al elemento a buscar
         vmejor[i] = x*(i+1);
         //Mitad vector igual y mitad diferente
-        if(i <= (tam/2)){
+        if(i <= (n/2)){
             vcualquiera[i] = x;
         }else{
             vcualquiera[i] = x*(i+1);
@@ -38,35 +37,35 @@ int main()
 
     cout << "VPEOR\n";
     cout << "----------------------------------------\n";
-    for(int i = 1; i <= tam; i++){
+    for(int i = 1; i <= n; i++){
         cout << vpeor[i] << " ";
     }
     cout << "\n----------------------------------------\n";
-    cout << "NVECES: " << Contar(vpeor,x,tam);
+    cout << "NVECES: " << Contar(vpeor,x,n);
 
     cout << "\nVMEJOR\n";
     cout << "----------------------------------------\n";
-    for(int i = 1; i <= tam; i++){
+    for(int i = 1; i <= n; i++){
         cout << vmejor[i] << " ";
     }
     cout << "\n----------------------------------------\n";
-    cout << "NVECES: " << Contar(vmejor,x,tam);
+    cout << "NVECES: " << Contar(vmejor,x,n);
 
     cout << "\nVCUALQUIERA\n";
     cout << "----------------------------------------\n";
-    for(int i = 1; i <= tam; i++){
+    for(int i = 1; i <= n; i++){
         cout << vcualquiera[i] << " ";
     }
     cout << "\n----------------------------------------\n";
-    cout << "NVECES: " << Contar(vcualquiera,x,tam);
+    cout << "NVECES: " << Contar(vcualquiera,x,n);
 
     return 0;
 }
 
-int Contar(int v[], int x, int tam){
+int Contar(int v[], int x, int n){
     //Veces que se encuentra x en v
     int nveces = 0;
-    for (int i = 0; i <= tam; i++){
+    for (int i = 0; i <= n; i++){
         //Si el valor de la posicion i del vector es igual a x nveces + 1
         if(v[i] == x){
             nveces ++;
